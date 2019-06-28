3c81624d54a30477af796f87b08bc8cc665a1b0e
Pedro Emanuel <pedro.azevedo@peixeurbano.com>
2019-06-28 17:05:00 -0300

    app for test with swiftUI

---
+++ SwifitUICollection/SwifitUICollection/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwifitUICollection
+//
+//  Created by Pedro Emanuel on 28/06/19.
+//  Copyright © 2019 Pedro Emanuel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
